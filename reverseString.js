b64536b2af7c014b150e9d84c20c08d144306483
devbaggett <devabaggett@gmail.com>
2020-11-09 14:36:35 -0800

    Updated reverseString.

--- reverseString.js
+++ reverseString.js
@@ -27,14 +27,14 @@
 //     return reversed_string;
 // }
 //
-// // FUNCTION #3
-// function reverseString(str) {
-//     let reversed_string = "";
-//     for (let character of str) {
-//         reversed_string = character + reversed_string;
-//     }
-//     return reversed_string;
-// }
+// FUNCTION #3
+function reverseString(str) {
+    let reversed_string = "";
+    for (let character of str) {
+        reversed_string = character + reversed_string;
+    }
+    return reversed_string;
+}
 //
 // // FUNCTION #4
 // function reverseString(str) {
@@ -42,9 +42,9 @@
 // }
 
 // FUNCTION #5
-function reverseString(str) {
-    return str.split('').reduce((reversed, char) => char + reversed, '');
-}
+// function reverseString(str) {
+//     return str.split('').reduce((reversed, char) => char + reversed, '');
+// }
 
 console.log(reverseString('apple'));
 
