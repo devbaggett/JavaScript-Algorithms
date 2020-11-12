80f0ba1ee214fafb0deecbdcb9c62d3c320ab3d3
devbaggett <devabaggett@gmail.com>
2020-11-11 22:18:30 -0800

    Added recursive isPalindrome funtion.

---
+++ recursive/isPalindrome.js
@@ -0,0 +1,20 @@
+// isPalindrome
+
+// Write a recursive function which returns true if the string passed to it is a palindrome (reads the same forward and
+// backward). Otherwise it returns false.
+
+function isPalindrome(str) {
+    if (str.length < 2) {
+        return true;
+    }
+    if (str[0] !== str[str.length - 1]) {
+        return false;
+    }
+    return isPalindrome(str.slice(1, str.length - 1));
+}
+
+console.log(isPalindrome("awesome")); // false
+console.log(isPalindrome("foobar")); // false
+console.log(isPalindrome("tacocat")); // true
+console.log(isPalindrome('amanaplanacanalpanama')); // true
+console.log(isPalindrome('amanaplanacanalpandemonium')); // false
