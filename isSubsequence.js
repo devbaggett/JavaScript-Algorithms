149329d8b4c2d16c96baf0fcb4eef046ad187a3d
devbaggett <devabaggett@gmail.com>
2020-11-30 20:16:23 -0800

    Updated isSubsequence function.

--- isSubsequence.js
+++ isSubsequence.js
@@ -10,11 +10,12 @@
 
 // iterative
 // function isSubsequence(str1, str2) {
-//     let i = 0;
-//     let j = 0;
 //     if (!str1) {
 //         return true;
 //     }
+//
+//     let i = 0;
+//     let j = 0;
 //     while (j < str2.length) {
 //         if (str2[j] === str1[i]) {
 //             i++;
