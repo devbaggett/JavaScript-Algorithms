8fbad4c3ee707a5fc556fdd96fcaf2676d229c58
devbaggett <devabaggett@gmail.com>
2020-11-30 16:10:40 -0800

    Added maxSubarraySub function.

---
+++ maxSubarraySum.js
@@ -0,0 +1,61 @@
+const {performance} = require('perf_hooks');
+
+// function maxSubarraySum(arr, n) {
+//     if (arr.length === 0) {
+//         return null;
+//     }
+//     let maxSum = 0;
+//     let temp = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         let slice = arr.slice(i, i + n);
+//         slice.forEach((num) => {
+//             temp += num;
+//             if (temp > maxSum) {
+//                 maxSum = temp;
+//             }
+//         });
+//         temp = 0;
+//     }
+//     return maxSum;
+// }
+
+// function maxSubarraySum(arr, num) {
+//     if (num > arr.length) {
+//         return null;
+//     }
+//     let max = -Infinity;
+//     for (let i = 0; i < arr.length - num + 1; i++) {
+//         let temp = 0;
+//         for (let j = 0; j < num; j++) {
+//             temp += arr[i + j];
+//         }
+//         if (temp > max) {
+//             max = temp;
+//         }
+//     }
+//     return max;
+// }
+
+// Sliding Window Pattern
+function maxSubarraySum(arr, num) {
+    let max = 0;
+    let temp;
+    if (arr.length < num) {
+        return null;
+    }
+    for (let i = 0; i < num; i++) {
+        max += arr[i];
+    }
+    temp = max;
+    for (let i = num; i < arr.length; i++) {
+        temp = temp - arr[i - num] + arr[i];
+        max = Math.max(max, temp);
+    }
+    return max;
+}
+
+
+let t0 = performance.now();
+console.log(maxSubarraySum([2, 6, 9, 2, 1, 8, 5, 6, 3], 3)); // 19
+let t1 = performance.now();
+console.log("Algorithm took " + (t1 - t0) + " milliseconds.");
