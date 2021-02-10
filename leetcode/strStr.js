1561fa3b1ce284bdfd1002e995b6dae7a35371f8
devbaggett <devabaggett@gmail.com>
2021-02-10 14:01:19 -0800

    Added optimized strStr() function.

--- leetcode/strStr.js
+++ leetcode/strStr.js
@@ -7,21 +7,41 @@
 // For the purpose of this problem, we will return 0 when needle is an empty string.
 // This is consistent to C's ' + 'strstr() and Java's indexOf().
 
+// slow
+// function strStr(haystack, needle) {
+//     if (needle.length < 1) {
+//         return 0;
+//     }
+//     if (haystack.length < needle.length) {
+//         return -1;
+//     }
+//     for (let i = 0; i < haystack.length; i++) {
+//         if (haystack[i] === needle[0]) {
+//             let j = 0;
+//             let k = i;
+//             while (haystack[k] === needle[j]) {
+//                 k++;
+//                 j++;
+//                 if (j === needle.length) {
+//                     return k - needle.length;
+//                 }
+//             }
+//         }
+//     }
+//     return -1;
+// }
+
+// fast
 function strStr(haystack, needle) {
     if (needle.length < 1) {
         return 0;
+    } else if (haystack.length < needle.length) {
+        return -1;
     }
-    for (let i = 0; i < haystack.length; i++) {
-        if (haystack[i] === needle[0]) {
-            let j = 0;
-            let k = i;
-            while (haystack[k] === needle[j]) {
-                k++;
-                j++;
-                if (j === needle.length) {
-                    return k - needle.length;
-                }
-            }
+    for (let i = 0; i <= haystack.length - needle.length; i++) {
+        let sub = haystack.slice(i, needle.length + i);
+        if (sub === needle) {
+            return i;
         }
     }
     return -1;
