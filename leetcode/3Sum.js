f47160c0a2ca37beb69143b0d499bf7d0d1eae73
devbaggett <devabaggett@gmail.com>
2021-02-18 19:51:03 -0800

    Updated function.

--- leetcode/3Sum.js
+++ leetcode/3Sum.js
@@ -27,58 +27,51 @@
 //     return res;
 // };
 
-// var threeSum = function(nums) {
-//     const result = [];
-//     nums.sort((a, b) => a - b);
-//     let left = 0;
-//     let right = nums.length - 1;
-//     for (let i = 0; i < nums.length; i++) {
-//         if (i > 0 && nums[i] === nums[i - 1]) continue;
-//         left = i + 1;
-//         right = nums.length - 1;
-//         let sum = 0;
-//         while (left < right) {
-//             sum = nums[i] + nums[left] + nums[right];
-//             if (sum === 0) {
-//                 result.push([nums[i], nums[left], nums[right]]);
-//                 left++;
-//                 right--;
-//                 while (left < right && nums[left] === nums[left - 1]) left++;
-//                 while (left < right && nums[right] === nums[right + 1]) right--;
-//             } else if (sum > 0) {
-//                 right--;
-//             } else {
-//                 left++
-//             }
-//         }
-//     }
-//     return result;
-// };
-
-// slowest, easiest to understand (imo)
-const threeSum = nums => {
+const threeSum = function (nums) {
     const result = [];
-    const set = new Set();
     nums.sort((a, b) => a - b);
     for (let i = 0; i < nums.length; i++) {
         if (i > 0 && nums[i] === nums[i - 1]) continue;
         let [left, right] = [i + 1, nums.length - 1];
         while (left < right) {
-            let [a, b, c] = [nums[i], nums[left], nums[right]];
-            let sum = a + b + c;
+            let sum = nums[i] + nums[left] + nums[right];
             if (sum === 0) {
-                if (!set.has(`${a}, ${b}, ${c}`)) {
-                    set.add(`${a}, ${b}, ${c}`);
-                    result.push([a, b, c]);
-                }
+                result.push([nums[i], nums[left], nums[right]]);
                 left++;
                 right--;
+                while (left < right && nums[left] === nums[left - 1]) left++;
+                while (left < right && nums[right] === nums[right + 1]) right--;
             } else if (sum > 0) right--;
             else left++;
         }
     }
     return result;
 };
+
+// slowest, easiest to understand (imo)
+// const threeSum = nums => {
+//     const result = [];
+//     const set = new Set();
+//     nums.sort((a, b) => a - b);
+//     for (let i = 0; i < nums.length; i++) {
+//         if (i > 0 && nums[i] === nums[i - 1]) continue;
+//         let [left, right] = [i + 1, nums.length - 1];
+//         while (left < right) {
+//             let [a, b, c] = [nums[i], nums[left], nums[right]];
+//             let sum = a + b + c;
+//             if (sum === 0) {
+//                 if (!set.has(`${a}, ${b}, ${c}`)) {
+//                     set.add(`${a}, ${b}, ${c}`);
+//                     result.push([a, b, c]);
+//                 }
+//                 left++;
+//                 right--;
+//             } else if (sum > 0) right--;
+//             else left++;
+//         }
+//     }
+//     return result;
+// };
 
 console.log(threeSum([-1, 0, 1, 2, -1, -4])); // [[-1, -1, 2], [-1, 0, 1]]
 console.log(threeSum([])); // []
